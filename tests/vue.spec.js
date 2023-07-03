7c2d9b0725a1c586f073e2a0fd53ebfc6df3dd69
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2023-07-03 12:58:14 +0200

    add vuew integrartion tests

---
+++ tests/vue.spec.js
@@ -0,0 +1,131 @@
+import { test, describe, expect } from 'vitest';
+import * as Vue from 'vue';
+import './CustomElement.js';
+
+describe('Lit', () => {
+    test('should update text content', async () => {
+        const container = document.createElement('div');
+        const app = Vue.createApp({
+            data() {
+                return {
+                    text: 'Text',
+                };
+            },
+            render() {
+                return Vue.h('custom-element', {}, [this.text]);
+            },
+            methods: {
+                updateText(value) {
+                    this.text = value;
+                },
+            },
+        });
+        app.mount(container);
+
+        const element = container.children[0];
+        const textNode = element.childNodes[0];
+        element.connectedCallback();
+
+        expect(container.childNodes.length).toBe(1);
+        expect(element.childNodes.length).toBe(1);
+        expect(element.parentNode).toBe(container);
+        expect(textNode.parentNode).toBe(element);
+        expect(textNode.textContent).toBe('Text');
+        expect(container.innerHTML).toBe('<custom-element><span>Text</span><div></div></custom-element>');
+
+        app._instance.proxy.updateText('Update');
+        await Vue.nextTick();
+
+        expect(container.childNodes.length).toBe(1);
+        expect(container.children[0]).toBe(element);
+        expect(element.childNodes.length).toBe(1);
+        expect(textNode.textContent).toBe('Update');
+        expect(container.innerHTML).toBe('<custom-element><span>Update</span><div></div></custom-element>');
+    });
+
+    test('should update text content with multiple text nodes', async () => {
+        const container = document.createElement('div');
+        const app = Vue.createApp({
+            data() {
+                return {
+                    text: 'Text',
+                };
+            },
+            render() {
+                return Vue.h('custom-element', {}, [this.text, ' ', 'children']);
+            },
+            methods: {
+                updateText(value) {
+                    this.text = value;
+                },
+            },
+        });
+        app.mount(container);
+
+        const element = container.children[0];
+        const textNode = element.childNodes[0];
+        element.connectedCallback();
+
+        expect(container.childNodes.length).toBe(1);
+        expect(element.parentNode).toBe(container);
+        expect(element.childNodes.length).toBe(3);
+        expect(textNode.parentNode).toBe(element);
+        expect(textNode.textContent).toBe('Text');
+        expect(container.innerHTML).toBe('<custom-element><span>Text children</span><div></div></custom-element>');
+
+        app._instance.proxy.updateText('Update');
+        await Vue.nextTick();
+
+        expect(container.childNodes.length).toBe(1);
+        expect(container.children[0]).toBe(element);
+        expect(element.childNodes.length).toBe(3);
+        expect(textNode.textContent).toBe('Update');
+        expect(container.innerHTML).toBe('<custom-element><span>Update children</span><div></div></custom-element>');
+    });
+
+    test('should update named slots', async () => {
+        const container = document.createElement('div');
+        const app = Vue.createApp({
+            data() {
+                return {
+                    title: true,
+                };
+            },
+            render() {
+                return Vue.h('custom-element', {}, [
+                    'Text ',
+                    this.title
+                        ? Vue.h('h1', { slot: 'children' }, 'Title')
+                        : Vue.h('h2', { slot: 'children' }, 'Subtitle'),
+                    '\n',
+                ]);
+            },
+            methods: {
+                updateTitle(value) {
+                    this.title = value;
+                },
+            },
+        });
+        app.mount(container);
+
+        const element = container.children[0];
+        const textNode = element.childNodes[0];
+        const lastNode = element.childNodes[2];
+        element.connectedCallback();
+
+        expect(element.childNodes.length).toBe(3);
+        expect(container.innerHTML).toBe(
+            '<custom-element><span>Text \n</span><div><h1 slot="children">Title</h1></div></custom-element>'
+        );
+
+        app._instance.proxy.updateTitle(false);
+        await Vue.nextTick();
+
+        expect(element.childNodes.length).toBe(3);
+        expect(element.childNodes[0]).toBe(textNode);
+        expect(element.childNodes[2]).toBe(lastNode);
+        expect(container.innerHTML).toBe(
+            '<custom-element><span>Text \n</span><div><h2 slot="children">Subtitle</h2></div></custom-element>'
+        );
+    });
+});
