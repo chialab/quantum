0aecf10b6414b2237f27353361ea21bf915c6afc
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2024-03-04 16:07:10 +0100

    Update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-import { fileURLToPath } from 'node:url';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [svelte()],
-    build: {
-        minify: true,
-        lib: {
-            entry: fileURLToPath(new URL('./src/index.js', import.meta.url)),
-            name: 'Quantum',
-            fileName: 'quantum',
-            formats: ['es'],
-        },
-    },
     test: {
         browser: {
             name: 'chrome',
