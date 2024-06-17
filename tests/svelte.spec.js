52babfb871c89c429e0247a65bf7e6c26d6b7de7
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2024-06-17 09:18:43 +0200

    Add support for svelte 5

--- tests/svelte.spec.js
+++ tests/svelte.spec.js
@@ -1,15 +1,12 @@
-import { cleanup, render } from '@testing-library/svelte';
-import { afterEach, describe, expect, test } from 'vitest';
+import { render } from '@testing-library/svelte';
+import { describe, expect, test } from 'vitest';
 import Test1 from './components/Test1.svelte';
 import Test2 from './components/Test2.svelte';
 import Test3 from './components/Test3.svelte';
 
 describe('Svelte', () => {
-    afterEach(cleanup);
-
     test('should update text content', async () => {
-        const body = document.createElement('div');
-        const { component } = render(
+        const { container, rerender } = render(
             Test1,
             {
                 props: {
@@ -17,11 +14,10 @@ describe('Svelte', () => {
                 },
             },
             {
-                container: body,
+                container: document.createElement('div'),
             }
         );
 
-        const container = body.children[0];
         const element = container.children[0];
         const textNode = element.childNodes[0];
         element.connectedCallback();
@@ -32,23 +28,22 @@ describe('Svelte', () => {
         expect(textNode.parentNode).toBe(element);
         expect(textNode.textContent).toBe('Text');
         expect(container.innerHTML).toBe(
-            '<custom-element><span>Text<!--isµ0--></span><div><!--isµ1--></div></custom-element><!--<Test1>-->'
+            '<custom-element><span>Text<!--isµ0--></span><div><!--isµ1--></div></custom-element>'
         );
 
-        await component.$set({ text: 'Update' });
+        await rerender({ text: 'Update' });
 
         expect(container.childNodes.length).toBe(2);
         expect(container.children[0]).toBe(element);
         expect(element.childNodes.length).toBe(1);
         expect(textNode.textContent).toBe('Update');
         expect(container.innerHTML).toBe(
-            '<custom-element><span>Update<!--isµ0--></span><div><!--isµ1--></div></custom-element><!--<Test1>-->'
+            '<custom-element><span>Update<!--isµ0--></span><div><!--isµ1--></div></custom-element>'
         );
     });
 
     test('should update text content with multiple text nodes', async () => {
-        const body = document.createElement('div');
-        const { component } = render(
+        const { container, rerender } = render(
             Test2,
             {
                 props: {
@@ -56,38 +51,36 @@ describe('Svelte', () => {
                 },
             },
             {
-                container: body,
+                container: document.createElement('div'),
             }
         );
 
-        const container = body.children[0];
         const element = container.children[0];
         const textNode = element.childNodes[0];
         element.connectedCallback();
 
         expect(container.childNodes.length).toBe(2);
         expect(element.parentNode).toBe(container);
-        expect(element.childNodes.length).toBe(3);
+        expect(element.childNodes.length).toBe(1);
         expect(textNode.parentNode).toBe(element);
-        expect(textNode.textContent).toBe('Text');
+        expect(textNode.textContent).toBe('Text children');
         expect(container.innerHTML).toBe(
-            '<custom-element><span>Text children<!--isµ0--></span><div><!--isµ1--></div></custom-element><!--<Test2>-->'
+            '<custom-element><span>Text children<!--isµ0--></span><div><!--isµ1--></div></custom-element>'
         );
 
-        await component.$set({ text: 'Update' });
+        await rerender({ text: 'Update' });
 
         expect(container.childNodes.length).toBe(2);
         expect(container.children[0]).toBe(element);
-        expect(element.childNodes.length).toBe(3);
-        expect(textNode.textContent).toBe('Update');
+        expect(element.childNodes.length).toBe(1);
+        expect(textNode.textContent).toBe('Update children');
         expect(container.innerHTML.replace(/\n\s+/g, ' ')).toBe(
-            '<custom-element><span>Update children<!--isµ0--></span><div><!--isµ1--></div></custom-element><!--<Test2>-->'
+            '<custom-element><span>Update children<!--isµ0--></span><div><!--isµ1--></div></custom-element>'
         );
     });
 
     test('should update named slots', async () => {
-        const body = document.createElement('div');
-        const { component } = render(
+        const { container, rerender } = render(
             Test3,
             {
                 props: {
@@ -95,27 +88,26 @@ describe('Svelte', () => {
                 },
             },
             {
-                container: body,
+                container: document.createElement('div'),
             }
         );
-        const container = body.children[0];
         const element = container.children[0];
         const textNode = element.childNodes[0];
         const lastNode = element.childNodes[2];
         element.connectedCallback();
 
-        expect(element.childNodes.length).toBe(3);
+        expect(element.childNodes.length).toBe(4);
         expect(container.innerHTML.replace(/\n\s+/g, ' ')).toBe(
-            '<custom-element><span>Text end<!--isµ0--></span><div><h1 slot="children">Title</h1><!--isµ1--></div></custom-element><!--<Test3>-->'
+            '<custom-element><span>Text <!----> end<!--isµ0--></span><div><h1 slot="children">Title</h1><!--isµ1--></div></custom-element>'
         );
 
-        await component.$set({ title: false });
+        await rerender({ title: false });
 
-        expect(element.childNodes.length).toBe(3);
+        expect(element.childNodes.length).toBe(4);
         expect(element.childNodes[0]).toBe(textNode);
         expect(element.childNodes[2]).toBe(lastNode);
         expect(container.innerHTML.replace(/\n\s+/g, ' ')).toBe(
-            '<custom-element><span>Text end<!--isµ0--></span><div><h2 slot="children">Subitle</h2><!--isµ1--></div></custom-element><!--<Test3>-->'
+            '<custom-element><span>Text <!----> end<!--isµ0--></span><div><h2 slot="children">Subitle</h2><!--isµ1--></div></custom-element>'
         );
     });
 });
